23cf806060459549096c6e6d42b9f390c186580d
niawjunior <pasupol@dome.cloud>
2019-04-20 23:08:33 +0700

    Update About

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  yearOld;
   constructor() { }
 
   ngOnInit() {
+    this.yearOld = moment().diff('1994-06-23', 'years');
   }
 
 }
