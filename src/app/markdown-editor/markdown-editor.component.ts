4a86c736ce28cdd4c703e9e8ad309da87a253e8f
niawjunior <pasupol@dome.cloud>
2019-05-16 14:27:24 +0700

    Update

--- src/app/markdown-editor/markdown-editor.component.ts
+++ src/app/markdown-editor/markdown-editor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MarkdownService } from 'ngx-markdown';
 import { EditorInstance, EditorOption } from 'angular-markdown-editor';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-markdown-editor',
@@ -15,13 +17,22 @@ export class MarkdownEditorComponent implements OnInit {
   showEditor = true;
   templateForm: FormGroup;
   editorOptions: EditorOption;
-
+  headers: Headers = new Headers({ 'Content-Type': 'text/plain' });
   constructor(
     private fb: FormBuilder,
-    private markdownService: MarkdownService
+    private markdownService: MarkdownService,
+    private afStorage: AngularFireStorage,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
+
+    this.http.get('https://firebasestorage.googleapis.com/v0/b/blog-40f93.appspot.com/o/article%2Ftest.text?alt=media&token=7b38fa44-4307-401e-953b-79b65ba64d9d', {
+    headers: new HttpHeaders().set('Content-Type', 'text/plain')
+  }).subscribe(value => {
+    console.log(value);
+  })
+    // this.afStorage.ref('article').child('test.text').getDownloadURL()
     this.editorOptions = {
       autofocus: false,
       iconlibrary: 'fa',
@@ -31,24 +42,27 @@ export class MarkdownEditorComponent implements OnInit {
       parser: (val) => this.parse(val)
     };
 
-    this.markdownText =
-      `### Markdown example
-      ---
-      This is an **example** where we bind a variable to the \`markdown\` component that is also bind to the editor.
-      #### example.component.ts
-      \`\`\`javascript
-      function hello() {
-        alert('Hello World');
-      }
-      \`\`\`
-      #### example.component.html
-      \`\`\`html
-      <textarea [(ngModel)]="markdown"></textarea>
-      <markdown [data]="markdown"></markdown>
-      \`\`\``;
+    // put the text completely on the left to avoid extra white spaces
+this.markdownText =
+`### Markdown example
+---
+This is an **example** where we bind a variable to the \`markdown\` component that is also bind to the editor.
+#### example.component.ts
+\`\`\`javascript
+function hello() {
+alert('Hello World');
+}
+\`\`\`
+#### example.component.html
+\`\`\`html
+<textarea [(ngModel)]="markdown"></textarea>
+<markdown [data]="markdown"></markdown>
+\`\`\``;
+
     this.buildForm(this.markdownText);
     this.onFormChanges();
   }
+
 
   buildForm(markdownText) {
     this.templateForm = this.fb.group({
@@ -56,6 +70,8 @@ export class MarkdownEditorComponent implements OnInit {
       isPreview: [true]
     });
   }
+
+  /** highlight all code found, needs to be wrapped in timer to work properly */
   highlight() {
     setTimeout(() => {
       this.markdownService.highlight();
@@ -65,6 +81,13 @@ export class MarkdownEditorComponent implements OnInit {
   hidePreview(e) {
     if (this.bsEditorInstance && this.bsEditorInstance.hidePreview) {
       this.bsEditorInstance.hidePreview();
+    }
+  }
+
+  showFullScreen(isFullScreen: boolean) {
+    if (this.bsEditorInstance && this.bsEditorInstance.setFullscreen) {
+      this.bsEditorInstance.showPreview();
+      this.bsEditorInstance.setFullscreen(isFullScreen);
     }
   }
 
